97bcfd58ae22a350041e1057c08980793cb671fa
Tharun Raj <THARUNRAJ2023@GMAIL.COM>
2025-05-18 00:47:43 +0530

    compleded

--- Url-frontEnd/src/module/Register.jsx
+++ Url-frontEnd/src/module/Register.jsx
@@ -238,9 +238,13 @@ const Register = () => {
             <div className="text-center mt-8">
               <p className="text-gray-600">
                 Already have an account?{" "}
-                <a href="/login" className="text-blue-600 hover:text-blue-800 font-medium transition-colors">
-                  Sign in
-                </a>
+                        <button
+                          onClick={() => navigate("/")}
+                          className="text-blue-600 hover:text-blue-800 font-medium transition-colors  cursor-pointer"
+                        >
+                          Sign in
+          </button>
+
               </p>
             </div>
           </form>
